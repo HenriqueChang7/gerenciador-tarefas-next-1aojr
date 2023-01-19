d6960c9476b9d0cbaa21f5279321281a043f9fbc
Henrique Renan Chang <henriquechang7@gmail.com>
2023-01-18 21:49:13 -0300

    feat: criando modal de sucesso no cadastro de usuario

--- pages/Cadastro_Usuario.tsx
+++ pages/Cadastro_Usuario.tsx
@@ -1,10 +1,14 @@
 import Router from "next/router";
 import { useState } from "react";
+import { Modal } from "react-bootstrap";
 import { executeRequest } from "../services/api";
 
 
 
 export default function CadastroUsuario() {
+
+    const [showModal, setShowModal] = useState(false);
+    const [mensagemModal, setMensagemModal] = useState('');
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -37,7 +41,8 @@ export default function CadastroUsuario() {
             const result = await executeRequest('user', 'post', body);
             if(result && result.data){
                 const obj = result.data;
-                Router.push('/');
+                setMensagemModal(result.data.msg);
+                setShowModal(true);
             }
         }catch(e : any){
             console.log(`Erro ao cadastrar usuário: ${e}`);
@@ -52,37 +57,53 @@ export default function CadastroUsuario() {
     }
 
     return (
-        <div className="container-login">
-            <img src="/logo.svg" alt="Logo Fiap" className="logo" />
-            <div className="form">
-                <h2 className="title_text">Inscrever-se</h2>    
-                {error && <p className="error">{error}</p>}
-                <div className="input">
-                    <img src="/user.svg" alt="Nome Icone" id="user_icon"/>
-                    <input type='text' placeholder="Nome"
-                        value={name}
-                        onChange={evento => setName(evento.target.value)}
-                    />
-                </div>
-                <div className="input">
-                    <img src="/mail.svg" alt="Login Icone" />
-                    <input type='text' placeholder="Login"
-                        value={email}
-                        onChange={evento => setEmail(evento.target.value)}
-                    />
-                </div>
-                <div className="input">
-                    <img src="/lock.svg" alt="Senha Icone" />
-                    <input type='password' placeholder="Senha"
-                        value={password}
-                        onChange={evento => setPassword(evento.target.value)}
-                    />
-                </div>
-                <button onClick={doCadastro} disabled={loading}>{loading ? '...Carregando': 'Cadastrar'}</button>
-                <div className="div_link">
-                    <span className="link_button" onClick={backToLogin}>Voltar</span>
+        <>
+            <div className="container-login">
+                <img src="/logo.svg" alt="Logo Fiap" className="logo" />
+                <div className="form">
+                    <h2 className="title_text">Inscrever-se</h2>    
+                    {error && <p className="error">{error}</p>}
+                    <div className="input">
+                        <img src="/user.svg" alt="Nome Icone" id="user_icon"/>
+                        <input type='text' placeholder="Nome"
+                            value={name}
+                            onChange={evento => setName(evento.target.value)}
+                        />
+                    </div>
+                    <div className="input">
+                        <img src="/mail.svg" alt="Login Icone" />
+                        <input type='text' placeholder="Login"
+                            value={email}
+                            onChange={evento => setEmail(evento.target.value)}
+                        />
+                    </div>
+                    <div className="input">
+                        <img src="/lock.svg" alt="Senha Icone" />
+                        <input type='password' placeholder="Senha"
+                            value={password}
+                            onChange={evento => setPassword(evento.target.value)}
+                        />
+                    </div>
+                    <button onClick={doCadastro} disabled={loading}>{loading ? '...Carregando': 'Cadastrar'}</button>
+                    <div className="div_link">
+                        <span className="link_button" onClick={backToLogin}>Voltar</span>
+                    </div>
                 </div>
             </div>
-        </div>
+            <Modal
+                show={showModal}
+                className="container-modal">
+                <Modal.Body>
+                    <div className="success_modal">
+                        <p className="normal_text">{mensagemModal}</p>
+                    </div>
+                </Modal.Body>
+                <Modal.Footer>
+                    <div className="button col-12">
+                        <span onClick={backToLogin}>voltar</span>
+                    </div>
+                </Modal.Footer>
+            </Modal>
+        </>
     );
 }
